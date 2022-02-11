70aea01919cf3b3cd0f887999171459eb01528a0
Rasmus <rasmus.ekstedt11@gmail.com>
2022-02-11 12:55:58 +0100

    Added Character class and examples

---
+++ src/resources.py
@@ -0,0 +1,21 @@
+# importa
+# global varaibles
+# classes
+class Character:
+    def __init__(self, name, health, damage, armor):
+        self.name = name
+        self.health = health
+        self.damage = damage
+        self.armor = armor
+
+    def __str__(self) -> str:
+        return f"Name: {self.name}\nHealth: {self.health}\nDamage: {self.damage}\nArmor: {self.armor}"
+
+
+# functions
+
+# main code
+
+
+
+
